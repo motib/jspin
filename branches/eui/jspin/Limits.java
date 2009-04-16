2facda688a4e403dd7f43293aa48af28e018deaf
moti.ben.ari <moti.ben.ari@4b11ffc2-f9ee-11dd-b507-8b4ecd5210be>
2009-04-16 07:26:04 +0000

    Limits options

---
+++ branches/eui/jspin/Limits.java
@@ -0,0 +1,101 @@
+/* Copyright 2005 by Mordechai (Moti) Ben-Ari. See copyright.txt. */
+/*
+ * Dialog for setting Common Options 
+ */
+
+package jspin;
+import javax.swing.*;
+import java.awt.event.*;
+
+class Limits extends JFrame implements ActionListener {
+  private JTextField stepsField, progressField, stackField, locationField;
+  private JLabel stepsLabel, progressLabel, stackLabel, locationLabel;
+  private JButton okButton, cancelButton;
+  private JLabel errorLabel;
+  private JPanel topPanel, buttonPanel, OKPanel;
+
+	Limits() {
+		buttonPanel = new JPanel();
+        buttonPanel.setLayout(new java.awt.GridLayout(4,2));
+    stepsLabel = new JLabel(Config.TotalSteps);
+    stepsField = new JTextField(Config.getStringProperty("TOTAL_STEPS"), 10);
+    buttonPanel.add(stepsLabel);
+    buttonPanel.add(stepsField);
+    progressLabel = new JLabel(Config.ProgressSteps);
+    progressField = new JTextField(Config.getStringProperty("PROGRESS_STEPS"), 10);
+    buttonPanel.add(progressLabel);
+    buttonPanel.add(progressField);
+    stackLabel = new JLabel(Config.StateStack);
+    stackField = new JTextField(Config.getStringProperty("STATE_STACK"), 10);
+    buttonPanel.add(stackLabel);
+    buttonPanel.add(stackField);
+    locationLabel = new JLabel(Config.LocationStack);
+    locationField = new JTextField(Config.getStringProperty("LOCATION_STACK"), 10);
+    buttonPanel.add(locationLabel);
+    buttonPanel.add(locationField);
+		OKPanel = new JPanel();
+        OKPanel.setLayout(new java.awt.GridLayout(1,2));
+		okButton = new JButton(Config.OK);
+		cancelButton = new JButton(Config.Cancel);
+    okButton.setMnemonic(Config.OKMN);
+    OKPanel.add(okButton);
+    OKPanel.add(cancelButton);
+		okButton.addActionListener(this);
+		cancelButton.addActionListener(this);
+    getRootPane().setDefaultButton(okButton);
+    getRootPane().registerKeyboardAction(this,
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                JComponent.WHEN_IN_FOCUSED_WINDOW);
+    errorLabel = new JLabel(" ");
+		topPanel = new JPanel();
+        topPanel.setLayout(new java.awt.GridLayout(1,1));
+    topPanel.add(errorLabel);
+    topPanel.setBorder(new javax.swing.border.LineBorder(java.awt.Color.BLUE));
+    buttonPanel.setBorder(new javax.swing.border.LineBorder(java.awt.Color.BLUE));
+    OKPanel.setBorder(new javax.swing.border.LineBorder(java.awt.Color.BLUE));
+    getContentPane().add(topPanel, java.awt.BorderLayout.NORTH);
+    getContentPane().add(buttonPanel, java.awt.BorderLayout.CENTER);
+    getContentPane().add(OKPanel, java.awt.BorderLayout.SOUTH);
+    setTitle(Config.Limits);
+    setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+    setSize(Config.getIntProperty("WIDTH")/4, 
+            Config.getIntProperty("HEIGHT")/3);
+    setLocationRelativeTo(null);
+		validate();
+    setVisible(true);
+  }
+
+  private int checkProperty(JTextField f) {
+    String s = f.getText();
+    try {
+      int i = Integer.parseInt(s);
+      if (i > 0) return i;
+      else throw new NumberFormatException();
+    }
+    catch (NumberFormatException e) {
+      errorLabel.setText("Value must be a positive integer");
+      return -1;
+    }
+  }
+
+  private void setProperty(int i, String c) {
+    Config.setIntProperty(c, i);
+  }
+
+  public void actionPerformed(ActionEvent e) {
+    int i1, i2, i3, i4;
+		if (e.getSource() == okButton) {
+      i1 = checkProperty(stepsField);    if (i1 == -1) return; 
+      i2 = checkProperty(progressField); if (i2 == -1) return; 
+      i3 = checkProperty(stackField);    if (i3 == -1) return; 
+      i4 = checkProperty(locationField); if (i4 == -1) return; 
+      setProperty(i1, "TOTAL_STEPS");
+      setProperty(i2, "PROGRESS_STEPS");
+      setProperty(i3, "STATE_STACK");
+      setProperty(i4, "LOCATION_STACK");
+      dispose();
+		}
+    else
+       dispose();
+	}
+}
