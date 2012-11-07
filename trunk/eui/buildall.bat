6ea1f3620436a2c5a7aac709e1a302f2f92dc782
moti.ben.ari@gmail.com <moti.ben.ari@gmail.com@4b11ffc2-f9ee-11dd-b507-8b4ecd5210be>
2012-11-07 09:18:19 +0000

    V1.8.2 - Fix bug that didn't display last state of simulation

--- trunk/eui/buildall.bat
+++ trunk/eui/buildall.bat
@@ -3,7 +3,6 @@ pdflatex -quiet eui-user
 pdflatex -quiet eui-user
 pdflatex -quiet eui-quick
 pdflatex -quiet eui-quick
-copy *.pdf c:\web\erigone\
 cd ..
 javac -target 1.5 eui\*.java
 jar cfm EUI.jar eui\MANIFEST.MF eui\*.class
--- trunk/eui/eui/Config.java
+++ trunk/eui/eui/Config.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 import java.util.*;
 
 public class Config {
-  static final String VERSION = "1.8";
+  static final String VERSION = "1.8.2";
 
   // Properties
   static Properties properties = new Properties();
