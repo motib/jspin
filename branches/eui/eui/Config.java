87df7ed9238f991bbaa837eaba672f1b9513b205
moti.ben.ari@gmail.com <moti.ben.ari@gmail.com@4b11ffc2-f9ee-11dd-b507-8b4ecd5210be>
2012-12-12 08:49:58 +0000

    Version 1.8.5: Erigone 3.2.5

--- branches/eui/eui/Config.java
+++ branches/eui/eui/Config.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 import java.util.*;
 
 public class Config {
-  static final String VERSION = "1.8.4";
+  static final String VERSION = "1.8.5";
 
   // Properties
   static Properties properties = new Properties();
